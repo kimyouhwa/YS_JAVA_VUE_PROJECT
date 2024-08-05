85d97772bf95820fe48ef75ddf6c55afeb2c431b
kimyouhwa <s_fan@naver.com>
2024-08-05 14:04:31 +0900

    11. vuex1

---
+++ src/views/UserView.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="">
+      <H1>사용자정보</H1>
+      <div >
+        <p><b>이름:</b>{{user.name}}</p>
+        <p><b>이메일:</b>{{user.email}}</p>
+      </div>
+      <button @click="saveUserData1"> 사용자정보저장1</button>
+      <button @click="saveUserData2"> 사용자정보저장2</button>
+    </div>
+  </template>
+  
+  <script>
+  import {useStore} from 'vuex';
+  import {computed} from 'vue';
+  
+  export default {
+    name: 'UserView',
+    setup() {
+      const store = useStore();
+      //vuex에서 user데이터를 가져오기
+      const user = computed(()=> store.state.user);
+  
+      //데이터1를 vuex에 저장하는 메서드
+      const saveUserData1 = ()=>{
+        const data = {name: '이순신', email:'ceo@sea.com'}
+        store.commit('setUser', data);
+      };
+  
+      //데이터2를 vuex에 저장하는 메서드
+      const saveUserData2 = ()=>{
+        const data = {name: '장발장', email:'thief@france.com'}
+        store.commit('setUser', data);
+      };
+  
+      return {
+        user, saveUserData1, saveUserData2
+      };
+    },
+  };
+  </script>
+  
+  <style scoped>
+  /* 스타일을 추가하세요 */
+  </style>
