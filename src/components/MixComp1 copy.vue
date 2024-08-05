a6aa2cc847a847e72a6ccee9a06c5ca43356648a
kimyouhwa <s_fan@naver.com>
2024-08-05 13:55:38 +0900

    9장 Misin

---
+++ src/components/MixComp1 copy.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="">
+      <h2>콤포넌트 1</h2>
+      <p>{{sharedData}}</p>
+      <button @click="sharedMethod">공유메서드</button>
+    </div>
+  </template>
+  
+  <script>
+  import {commonMixin} from '@/mixins/commonMixIn';
+  export default {
+    name: 'MixComp1',
+    mixins: [commonMixin],
+    mounted(){
+      console.log(' 코포넌트1 mounted');    
+    },
+    unmounted(){
+      console.log(' 코포넌트1 unmounted');    
+    },
+  };
+  </script>
+  
+  <style scoped>
+  /* 스타일을 추가하세요 */
+  </style>
---
+++ src/components/MixComp2.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="">
+      <h2>콤포넌트 2</h2>
+      <p>{{sharedData}}</p>
+      <button @click="sharedMethod">공유메서드</button>
+    </div>
+  </template>
+  
+  <script>
+  import {commonMixin} from '@/mixins/commonMixIn';
+  export default {
+    name: 'MixComp2',
+    mixins: [commonMixin],
+    mounted(){
+      console.log(' 코포넌트2 mounted');    
+    },
+    unmounted(){
+      console.log(' 코포넌트2 unmounted');    
+    },
+  };
+  </script>
+  
+  <style scoped>
+  /* 스타일을 추가하세요 */
+  </style>
