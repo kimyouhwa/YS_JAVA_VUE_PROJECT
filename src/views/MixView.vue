cf3544f7e3554983a21055f3e531e70b6031fe00
kimyouhwa <s_fan@naver.com>
2024-08-05 14:04:05 +0900

    9장 Misin

---
+++ src/views/MixView.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="">
+      <MixComp1/>
+      <MixComp2/>
+    </div>
+  </template>
+  
+  <script>
+  import MixComp1 from '@/components/MixComp1.vue';
+  import MixComp2 from '@/components/MixComp2.vue';
+  export default {
+    name: 'MixView',
+    components: {
+      MixComp1,
+      MixComp2
+    },
+  };
+  </script>
+  
+  <style scoped>
+  /* 스타일을 추가하세요 */
+  </style>
